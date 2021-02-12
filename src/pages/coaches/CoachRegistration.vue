7516cf6c1dd96a5752ce801d0fefc5060fdf3f54
Dan Trusca <truscadani@yahoo.com>
2021-02-12 16:15:10 +0200

    register as coach: the form

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,5 +1,17 @@
 <template>
   <section>
-    Register as Coach
+    <base-card>
+      <h2>Register as a Coach now</h2>
+      <coach-form></coach-form>
+    </base-card>
   </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: {
+    CoachForm
+  }
+};
+</script>
