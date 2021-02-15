83139c0288d3738791dd65c5be1c99fd54ef3643
Dan Trusca <truscadani@yahoo.com>
2021-02-15 12:44:09 +0200

    sending a PUT Http Request to store coach data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,8 +1,9 @@
 export default {
-  registerCoach(context, payload) {
+  async registerCoach(context, payload) {
+    const userId = context.rootGetters.userId;
     const coachData = {
       //   id: new Date().toISOString(),
-      id: context.rootGetters.userId,
+      // id: context.rootGetters.userId,
       firstName: payload.first,
       lastName: payload.last,
       areas: payload.areas,
@@ -10,6 +11,23 @@ export default {
       hourlyRate: payload.rate
     };
 
-    context.commit('registerCoach', coachData);
+    const response = await fetch(
+      `https://vue-hhtp-demo-9b5cb-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData)
+      }
+    );
+
+    //const responseData=await response.json();
+
+    if (!response.ok) {
+      //error message
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    });
   }
 };
