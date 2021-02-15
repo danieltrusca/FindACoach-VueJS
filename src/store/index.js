bdf9f433b7932a73e451b592afa567ee7e1ba180
Dan Trusca <truscadani@yahoo.com>
2021-02-15 19:45:04 +0200

    preparing VUEX for authentication

--- src/store/index.js
+++ src/store/index.js
@@ -2,21 +2,13 @@ import { createStore } from 'vuex';
 
 import CoachesModules from './modules/coaches/index.js';
 import RequestsModules from './modules/requests/index.js';
+import AuthModules from './modules/auth/index.js';
 
 const store = createStore({
   modules: {
     coaches: CoachesModules,
-    requests: RequestsModules
-  },
-  state() {
-    return {
-      userId: 'c3'
-    };
-  },
-  getters: {
-    userId(state) {
-      return state.userId;
-    }
+    requests: RequestsModules,
+    auth: AuthModules
   }
 });
 
